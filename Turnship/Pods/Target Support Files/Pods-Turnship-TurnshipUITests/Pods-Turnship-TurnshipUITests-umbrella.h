e67588762ac7d33e9ae7d16efa7ae14e46484adb
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2021-11-09 17:33:37 -0500

    added workspace

---
+++ Turnship/Pods/Target Support Files/Pods-Turnship-TurnshipUITests/Pods-Turnship-TurnshipUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Turnship_TurnshipUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Turnship_TurnshipUITestsVersionString[];
+
---
+++ Turnship/Pods/Target Support Files/Pods-Turnship/Pods-Turnship-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Turnship : NSObject
+@end
+@implementation PodsDummy_Pods_Turnship
+@end
