d87198c3fbfc9b36601e307fa6e259529954c06d
rijadkasumi <rijadd8@gmail.com>
2021-11-11 23:18:24 -0500

    Rijad Story Board added

--- Turnship/Turnship/ViewController.swift
+++ Turnship/Turnship/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func goToOther(_ sender: Any) {
+        
+        let storyboard = UIStoryboard(name: "Rijad", bundle: nil)
+          let view = storyboard.instantiateViewController(withIdentifier: "Rijad")
+          self.present(view, animated: true, completion: nil)
+    }
+    
 }
 
