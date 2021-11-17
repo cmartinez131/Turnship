50fc5cefb68ceae01dccac6ced757831048e6731
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2021-11-17 16:23:31 -0500

    added propoerties to user object

---
+++ Turnship/Turnship/StudentProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StudentProfileViewController.swift
+//  Turnship
+//
+//  Created by Christopher Martinez on 11/17/21.
+//
+
+import Parse
+import UIKit
+
+class StudentProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
